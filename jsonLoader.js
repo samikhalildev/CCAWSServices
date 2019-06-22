b007f90d3e9059affc246989dcd48f8f009019f3
samikhalildev <sami.khalil.dev@gmail.com>
2019-06-22 23:07:22 +1000

    Add unit tests

--- jsonLoader.js
+++ jsonLoader.js
@@ -1,12 +1,16 @@
+let url = 'https://us-west-2.cloudconformity.com/v1/services';
 let servicesMap = {};
-let servicesAPI = 'https://us-west-2.cloudconformity.com/v1/services';
+let unitTests = {};
 
-document.addEventListener('DOMContentLoaded', () => fetchAPI());
+document.addEventListener('DOMContentLoaded', () => fetchAPI(url));
 
-const fetchAPI = () => {
-  fetch(servicesAPI)
+const fetchAPI = url => {
+  if (isEmpty(url)) return;
+
+  fetch(url)
     .then(res => res.json())
     .then(json => {
+      if (isEmpty(json.data) || isEmpty(json.included)) return;
       let services = json.data;
       let content = json.included;
 
@@ -16,17 +20,23 @@ const fetchAPI = () => {
 
       loadDataToDOM();
     });
+
+  return true;
 };
 
 const getServiceContent = (service, content) => {
-  let title = service.attributes.name;
-  let data = service.relationships.rules.data;
-  servicesMap[title] = [];
+  try {
+    let title = service.attributes.name;
+    let data = service.relationships.rules.data;
+    servicesMap[title] = [];
 
-  data.forEach(d => {
-    let res = content.filter(c => c.id == d.id)[0].attributes;
-    servicesMap[title].push(res);
-  });
+    data.forEach(d => {
+      let res = content.filter(c => c.id == d.id)[0].attributes;
+      servicesMap[title].push(res);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const loadDataToDOM = () => {
@@ -35,6 +45,8 @@ const loadDataToDOM = () => {
     console.log(servicesMap[key]);
 
     let res = document.getElementById('res');
+    let loading = document.getElementById('loading');
+
     let row = '';
     let count = res.childElementCount;
     let addNewRow = res.children[count - 1].childElementCount === 2;
@@ -61,6 +73,7 @@ const loadDataToDOM = () => {
 
     column.appendChild(ul);
 
+    loading.innerText = '';
     if (addNewRow) {
       row.appendChild(column);
       res.appendChild(row);
@@ -69,3 +82,28 @@ const loadDataToDOM = () => {
     }
   }
 };
+
+const isEmpty = value => {
+  return (
+    value === undefined ||
+    value === null ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0)
+  );
+};
+
+// UNIT TESTS
+
+unitTests.isEmpty = function(method) {
+  return method('stuff') === true;
+};
+
+unitTests.fetchAPI = function(method) {
+  return method(url) === true;
+};
+
+unitTests.getServiceContent = function(method) {
+  return method('service', 'content') === true;
+};
+
+//console.log(unitTests.isEmpty(isEmpty));
