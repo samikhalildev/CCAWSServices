85baacc0d6bc02a79d5e2dae4b3e63f043265229
samikhalildev <sami.khalil.dev@gmail.com>
2019-06-22 21:28:48 +1000

    Manipulate DOM

--- jsonLoader.js
+++ jsonLoader.js
@@ -3,7 +3,7 @@ let servicesAPI = 'https://us-west-2.cloudconformity.com/v1/services';
 
 document.addEventListener('DOMContentLoaded', () => fetchAPI());
 
-fetchAPI = () => {
+const fetchAPI = () => {
   fetch(servicesAPI)
     .then(res => res.json())
     .then(json => {
@@ -14,14 +14,11 @@ fetchAPI = () => {
         getServiceContent(service, content);
       });
 
-      for (let key in servicesMap) {
-        console.log(key);
-        console.log(servicesMap[key]);
-      }
+      loadDataToDOM();
     });
 };
 
-getServiceContent = (service, content) => {
+const getServiceContent = (service, content) => {
   let title = service.attributes.name;
   let data = service.relationships.rules.data;
   servicesMap[title] = [];
@@ -30,4 +27,45 @@ getServiceContent = (service, content) => {
     let res = content.filter(c => c.id == d.id)[0].attributes;
     servicesMap[title].push(res);
   });
+};
+
+const loadDataToDOM = () => {
+  for (let key in servicesMap) {
+    console.log(key);
+    console.log(servicesMap[key]);
+
+    let res = document.getElementById('res');
+    let row = '';
+    let count = res.childElementCount;
+    let addNewRow = res.children[count - 1].childElementCount === 2;
+
+    if (addNewRow) {
+      row = document.createElement('div');
+      row.className = 'row';
+    }
+
+    let column = document.createElement('div');
+    let title = document.createElement('h3');
+    let ul = document.createElement('ul');
+    let li = document.createElement('li');
+
+    column.className = 'col-sm';
+
+    title.innerText = key;
+    column.appendChild(title);
+
+    servicesMap[key].forEach(element => {
+      li.innerText = element.title;
+      ul.appendChild(li);
+    });
+
+    column.appendChild(ul);
+
+    if (addNewRow) {
+      row.appendChild(column);
+      res.appendChild(row);
+    } else {
+      res.children[--count].appendChild(column);
+    }
+  }
 };
